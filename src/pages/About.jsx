5d3676c6a8f8679d5daa691f169e062cb22a14ac
yashasvi <vermayashasvi@gmail.com>
2026-08-22 12:45:05 +0530

    About hero: replace with latest image; remove old versions

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ import TiltImage from '../components/TiltImage'
 import { values } from '../data/site'
 import imgStory from '../assets/about inside image.jpg'
 import imgCtaBg from '../assets/products/Parametric wall panel.jpeg'
-import imgHero from '../assets/about updated hero image.png'
+import imgHero from '../assets/about latest hero image.png'
 
 function MissionVision() {
   const cards = [
